7b0430287e9f07c452bd59e3dbfb89ea40dbef51
Stanley <stanley.traub@gmail.com>
2020-03-16 15:08:03 -0700

    User Auth done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
+import Main from './pages/main/Main'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NavBar from './components/nav/navbar'
+import SpacesContainer from './components/spaces/space_index_container'
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path='/' component={Main} />
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+      <Route exact path="/spaces" component={SpacesContainer} />
+    </Router>
   );
 }
 
